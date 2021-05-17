8b188ec4356b41de158b714dc595cb55e4f8f93d
Riccardo Mirarchi <ric.mirarchi@gmail.com>
2021-05-17 21:42:37 +0200

    main changes

--- dataset_manipulation.py
+++ dataset_manipulation.py
@@ -9,10 +9,14 @@ colors_matching = {'Red': 'Red', 'Blue': 'Blue', 'Blue-White': 'Blue-White',
                    'Orange-Red': 'Orange', 'Pale yellow orange': 'Orange', 'Color': 'Color',
                    'Blue white': 'Blue-White'}
 
+type_matching = {'0': 'Red Dwarf', '1': 'Brown Dwarf', '2': 'White Dwarf',
+                 '3': 'Main Sequence', '4': 'Super Giants', '5': 'Hyper Giants', 'Type': 'Type'}
+
 file = csv.reader(open('Stars.csv'))
 lines = list(file)
 
 for line in lines:
+    line[6] = type_matching[line[6]]
     line[4] = colors_matching[line[4]]
 
 # write lines back in a csv file
